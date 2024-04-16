b5e5b432e7753f476d8c86da56db942100841dbc
Ivan Moskalev <moskalev-i@protei-lab.ru>
2024-04-16 16:56:49 +0300

    Delete perfect margin

--- packages.el
+++ packages.el
@@ -295,28 +295,6 @@
 
 
 
-;; Perfect-margin
-(use-package perfect-margin
-	:custom
-    (perfect-margin-visible-width 160)
-	:config
-	(toggle-truncate-lines)
-	;; enable perfect-mode
-    (perfect-margin-mode t)
-	;; auto-center minibuffer windows
-    (setq perfect-margin-ignore-filters nil)
-	;; auto-center special windows
-    (setq perfect-margin-ignore-regexps nil)
-	;; add additinal bding on margin area
-    (dolist (margin '("<left-margin> " "<right-margin> "))
-		(global-set-key (kbd (concat margin "<mouse-1>")) 'ignore)
-		(global-set-key (kbd (concat margin "<mouse-3>")) 'ignore)
-		(dolist (multiple '("" "double-" "triple-"))
-			(global-set-key (kbd (concat margin "<" multiple "wheel-up>")) 'mwheel-scroll)
-			(global-set-key (kbd (concat margin "<" multiple "wheel-down>")) 'mwheel-scroll))))
-
-
-
 ;; Blamer
 (use-package blamer
 	:defer 20
